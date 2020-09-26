265b701d15aed9498c806c7d18e5b1941c19294e
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-09-26 20:53:23 +0900

    validation設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :purchase
+  has_many :items
+
+  with_options presence: true do
+      validates :nickname, format { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+      validates :email
+      validates :password
+      validates :password_confirm
+      validates :family_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+      validates :first_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+      validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+      validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+      validates :birthday
+
 end
