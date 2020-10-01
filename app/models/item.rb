c6fc37e38f130e4df1a6d48bf20024296cc461a1
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-10-01 14:04:00 +0900

    アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+
+
+belongs_to :user
+has_one :purchase
+
+with_options presence: true do
+    validates :password, length: {minimum: 6 }, format: { with: VALID_PASSWORD_REGEX}
+    
+end
 end
