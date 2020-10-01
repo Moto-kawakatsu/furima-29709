d52807e245bdf030e7d554611846d18eac6cecef
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-10-01 13:07:49 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,22 @@ class User < ApplicationRecord
          
          has_many :items
          has_many :purchases
-        VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{1,50}+\z/
-        validates :password, presence: true, length: {minimum: 6 }, format: { with: VALID_PASSWORD_REGEX}
-         validates :email, presence: true
-         validates :nickname, presence: true
-         validates :birthday, presence: true
-         validates :family_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-         validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-         validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
-         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
 
+
+        VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{1,50}+\z/
+        kanji = /\A[ぁ-んァ-ン一-龥]/
+        kana = /\A[ァ-ヶー－]+\z/
+        kanji_message = "は全角で入力してください。"
+        kana_message = "は全角カタカナで入力して下さい。"
+
+       with_options presence: true do
+          validates :password, length: {minimum: 6 }, format: { with: VALID_PASSWORD_REGEX}
+          validates :email
+          validates :nickname
+          validates :birthday
+          validates :family_name_kanji, format: { with: kanji , message: kanji_message}
+          validates :first_name_kanji, format: { with: kanji, message: kanji_message}
+          validates :family_name_kana, format: { with: kana, message: kana_message}
+          validates :first_name_kana, format: { with: kana, message: kana_message}
+       end
 end
