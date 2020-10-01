2952fd4fb4c456a7b607c12bc123361717a104f6
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-10-01 16:12:10 +0900

    itemsコントローラー作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,46 +1,39 @@
 class ItemsController < ApplicationController
+    class ItemsController < ApplicationController
     
-    # itemテーブルの全てのレコードを@itemsに代入した
-    def index
-        @items = Item.all
-    end
+        # itemテーブルの全てのレコードを@itemsに代入した
+        def index
+            @items = Item.all
+        end
+        
+        def new
+            @item = Item.new
+        end
     
-    def new
-        @item = Item.new
+        def create
+            @item = Item.new(item_params)
+    
+        end
+    
+    
+    
+        private
+    
+        def item_params
+            params.require(:item).permit(
+                :user,
+                :name,
+                :description,
+                :category_id,
+                :condition_id
+                :shipping_fee_id
+                :ship_from_address_id
+                :shipping_days_id
+                :price
+            ).merge(
+                user_id: current_user.id,
+            )
+        end
+        end    
     end
-
-    def create
-        @item = Item.new(item_params)
-
-    end
-
-
-
-    private
-
-    def item_params
-        params.require(:item).permit(
-            :user,
-            :name,
-            :description,
-            :category_id,
-            :condition_id
-            :shipping_fee_id
-            :ship_from_address_id
-            :shipping_days_id
-            :price
-        ).merge(
-            user_id: current_user.id,
-        )
-    end
-
-
-
-
-    end
-
-
-
-
-
 end
