d5b121aaa85f6b04eb57e382844cd0db2e9a9675
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-10-02 19:04:17 +0900

    Active Hash の記述  (修正)

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,20 @@ class Item < ApplicationRecord
     extend ActiveHash::Associations::ActiveRecordExtensions
 
 belongs_to_active_hash :category
+belongs_to_active_hash :condition
+belongs_to_active_hash :shipping_fee
+belongs_to_active_hash :ship_from_address
+belongs_to_active_hash :shipping_days
 belongs_to :user
 has_one :purchase
 has_one_attached :image
 
 
 validates :user, :name, :category, :description, :category, :condition, :shipping_fee, :ship_from_address, :shipping_days, :price,  presence: true
-validates :genre_id, numericality: { other_than: 1 } 
+
+validates :category_id, numericality: { other_than: 1 }
+validates :condition_id, numericality: { other_than: 1 } 
+validates :shipping_fee_id, numericality: { other_than: 1 } 
+validates :ship_from_id, numericality: { other_than: 1 } 
+validates :shipping_days_id, numericality: { other_than: 1 }  
 end
---
+++ spec/factories/ship_from_addresses.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :ship_from_address do
+    
+  end
+end
