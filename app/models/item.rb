d6987bef467a7fadd099a0dd6e92569d3a90e2b8
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-10-03 11:25:14 +0900

    item.rbのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,28 @@ has_one :purchase
 has_one_attached :image
 
 
-validates :user, :name, :category, :description, :category, :condition, :shipping_fee, :ship_from_address, :shipping_days, :price,  presence: true
+# except_one = numericality: { other_than: 1}
 
-validates :category_id, numericality: { other_than: 1 }
-validates :condition_id, numericality: { other_than: 1 } 
-validates :shipping_fee_id, numericality: { other_than: 1 } 
-validates :ship_from_address_id, numericality: { other_than: 1 } 
-validates :shipping_days_id, numericality: { other_than: 1 }  
+with_options presence: true do
+    validates :user
+    validates :name
+    validates :category
+    validates :description
+    validates :category_id, numericality: { other_than: 1}
+    validates :condition_id, numericality: { other_than: 1}
+    validates :shipping_fee_id, numericality: { other_than: 1} 
+    validates :ship_from_address_id, numericality: { other_than: 1}
+    validates :shipping_days_id, numericality: { other_than: 1}
+    validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9999999 }
 end
+end
+
+# 前の記述 
+# validates :user, :name, :category, :description, :category, :condition, :shipping_fee, :ship_from_address, :shipping_days, :price,  presence: true
+
+# validates :category_id, numericality: { other_than: 1 }
+# validates :condition_id, numericality: { other_than: 1 } 
+# validates :shipping_fee_id, numericality: { other_than: 1 } 
+# validates :ship_from_address_id, numericality: { other_than: 1 } 
+# validates :shipping_days_id, numericality: { other_than: 1 }  
+# end
