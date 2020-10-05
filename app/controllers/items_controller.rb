d57c9ddde5a62df13b7fd9ef27bdd6a6184670b7
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-10-05 15:27:17 +0900

    before_action :set_itemの記述を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
         before_action :authenticate_user!, except: [:index, :show]
-      
+        before_action :set_item, only: [:show]
         def index
             @items = Item.all.order("created_at DESC")
         end
@@ -20,7 +20,6 @@ class ItemsController < ApplicationController
     
 
         def show 
-            @item = Item.find(params[:id])
         end
         
         private
@@ -39,4 +38,9 @@ class ItemsController < ApplicationController
                 :image
             ).merge(user_id: current_user.id)
         end    
+
+        def set_item
+            @item = Item.find(params[:id])
+        end
+
     end
