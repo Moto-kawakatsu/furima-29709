f4a8d23fd6d0b2d1136cd3896a6324c377787286
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-10-06 23:28:17 +0900

    クレジットカード支払いのための記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,34 @@
 class OrdersController < ApplicationController
     def index
-    #    @order = Order.find_by_id(params[:id])
          @item = Item.find(params[:item_id])
+         @order = Order.new
     end
 
     def create
-        # if @order.save
-        #     redirect_to item_orders_path
-        # else 
-        #     render :new
-        # end
+        # @order = Order.new(order_params)
+        @order = PayForm.new()order_params
+        if @order.valid?
+            pay_item
+            @order.save
+            return redirect_to root_path
+        else
+            render 'index'
+        end
     end
-
 
     private
     
     def order_params
-        params.require(:order).permit(:price, :item_id).merge(item_id:@item.id)
+        params.require(:order).permit(:price).merge(token: params[:token])
+        # params.require(:order).permit(:price, :item_id, :token).merge(item_id:@item.id)
     end
+
+    def pay_item
+        Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+        Payjp::Charge.create(
+          amount: order_params[:price],  # 商品の値段
+          card: order_params[:token],    # カードトークン
+          currency:'jpy'                 # 通貨の種類(日本円)
+        )
+      end
 end
