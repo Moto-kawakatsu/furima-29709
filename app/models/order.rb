19751054f86051cd66abd754a6294838882aff13
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-10-06 23:27:40 +0900

    orderモデル バリデーションの追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,9 @@
 class Order < ApplicationRecord
     validates :price, presence: true
+    validates :token, presence: true
 
     belongs_to :user
     belongs_to :item
+
+    attr_accessor :token
 end
