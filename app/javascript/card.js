595437a08dd221e17736b2c7fb21a3f188c2ecf1
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-10-09 16:45:36 +0900

    デバック用の記述を削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,10 +13,8 @@ const pay = ()=> {
             exp_month: formData.get("buy[exp_month]"),
             exp_year: `20${formData.get("buy[exp_year]")}`,
           };
-          console.log(card)
+  
           Payjp.createToken(card, (status, response) => {
-            console.log(status)    
-            console.log(response)
 
             if (status == 200) {
               const token = response.id;
