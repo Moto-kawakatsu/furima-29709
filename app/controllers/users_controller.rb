ab90d16d315fd5f179e120acabeff224a2f1635b
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-10-26 18:53:33 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
     private
 
-def user_params
+ def user_params
         params.require(:user).permit(
             :nickname,
             :email,
