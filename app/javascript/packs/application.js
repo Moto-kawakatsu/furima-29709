ff4f04f143cf97770e108fc184ae53742a45cd4c
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-12-12 08:42:22 +0900

    検索ページでの表示を追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require("../price")
 require("../memo")
 require("../card")
+require("../result")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
---
+++ app/javascript/result.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+document.getElementById('yes').onclick = function() {
+  document.getElementById('res').innerHTML = 'お買い物をお楽しみください！';
+};
+};
